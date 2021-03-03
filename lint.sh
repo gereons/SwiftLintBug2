dedce3ac0af9dccf34d4e1f1b549cae0dcab791a
Gereon Steffens <gereon@steffens.org>
2021-03-03 09:18:41 +0100

    use --compile-commands

--- lint.sh
+++ lint.sh
@@ -2,4 +2,14 @@
 xcodebuild clean -project SwiftLintTest.xcodeproj -scheme SwiftLintTest
 rm -rf ~/Library/Developer/Xcode/DerivedData/SwiftLintTest-*
 xcodebuild -project SwiftLintTest.xcodeproj -scheme SwiftLintTest >xcodebuild.log
-swiftlint analyze --compiler-log-path xcodebuild.log
+
+# swiftlint analyze --compiler-log-path xcodebuild.log
+
+if [ ! -d XcodeCompilationDatabase ]
+then
+    git clone https://github.com/jerrymarino/XcodeCompilationDatabase
+    (cd XcodeCompilationDatabase; make release)
+fi
+
+XcodeCompilationDatabase/.build/release/XCodeCompilationDatabase xcodebuild.log
+swiftlint analyze --compile-commands compile_commands.json
